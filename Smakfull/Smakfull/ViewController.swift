ba14edcfe1b7b1f2860deb2058aec30be483a0ec
Magdusz <goferek23@gmail.com>
2018-04-03 17:43:12 +0200

    add gitignore, add initial project

---
+++ Smakfull/Smakfull/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smakfull
+//
+//  Created by Magdusz on 03.04.2018.
+//  Copyright © 2018 com.mcpusz.smakfull. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
