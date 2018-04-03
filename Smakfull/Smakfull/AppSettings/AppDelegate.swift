d56053ae9d4adbea434635305220cde1acf71ef7
Magdusz <goferek23@gmail.com>
2018-04-03 18:14:02 +0200

    add initial screen UI components, add podfile

---
+++ Smakfull/Smakfull/AppSettings/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Smakfull
+//
+//  Created by Magdusz on 03.04.2018.
+//  Copyright © 2018 com.mcpusz.smakfull. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+}
+
---
+++ Smakfull/Smakfull/Modules/InitialScreen/View/InitialViewController.swift
@@ -0,0 +1,24 @@
+//
+//  InitialViewController.swift
+//  Smakfull
+//
+//  Created by Magdusz on 03.04.2018.
+//  Copyright © 2018 com.mcpusz.smakfull. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupView()
+    }
+    
+    private func setupView() {
+        
+    }
+
+}
